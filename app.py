317cabefa9f959169aecba593b04c6b49751c767
DhruvaEshwar <dhruvase06@gmail.com>
2026-02-07 07:51:07 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+# ------------------ Page Config ------------------
+st.set_page_config(
+    page_title="Love in the Line of Fire",
+    layout="centered"
+)
+
+# ------------------ Session State ------------------
+if "page" not in st.session_state:
+    st.session_state.page = "Home"
+
+# ------------------ Top Menu Button ------------------
+col1, col2 = st.columns([1, 10])
+
+with col1:
+    if st.button("☰"):
+        st.session_state.menu_open = not st.session_state.get("menu_open", False)
+
+# ------------------ Sidebar Menu ------------------
+if st.session_state.get("menu_open", False):
+    with st.sidebar:
+        st.title("Menu")
+
+        if st.button("🏠 Home"):
+            st.session_state.page = "Home"
+
+        if st.button("📖 Journey of a Young Mind"):
+            st.session_state.page = "Journey"
+
+# ------------------ Pages ------------------
+if st.session_state.page == "Home":
+    st.markdown("<h1 style='text-align:center;'>Love in the Line of Fire</h1>", unsafe_allow_html=True)
+
+elif st.session_state.page == "Journey":
+    st.markdown("<h1>Journey of a Young Mind</h1>", unsafe_allow_html=True)
+    st.write("📅 **Will be uploaded on 14th Feb, 2026**")
+
