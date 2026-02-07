df661f3154f8438b9a32706d69c18cb0756d0fd4
DhruvaEshwar <dhruvase06@gmail.com>
2026-02-07 08:23:32 +0000

    f

--- app.py
+++ app.py
@@ -10,29 +10,23 @@ st.set_page_config(
 if "page" not in st.session_state:
     st.session_state.page = "Home"
 
-# ------------------ Top Menu Button ------------------
-col1, col2 = st.columns([1, 10])
+# ------------------ Sidebar Menu (Streamlit default) ------------------
+with st.sidebar:
+    st.title("Menu")
 
-with col1:
-    if st.button("☰"):
-        st.session_state.menu_open = not st.session_state.get("menu_open", False)
+    if st.button("🏠 Home"):
+        st.session_state.page = "Home"
 
-# ------------------ Sidebar Menu ------------------
-if st.session_state.get("menu_open", False):
-    with st.sidebar:
-        st.title("Menu")
-
-        if st.button("🏠 Home"):
-            st.session_state.page = "Home"
-
-        if st.button("📖 Journey of a Young Mind"):
-            st.session_state.page = "Journey"
+    if st.button("📖 Journey of a Young Mind"):
+        st.session_state.page = "Journey"
 
 # ------------------ Pages ------------------
 if st.session_state.page == "Home":
-    st.markdown("<h1 style='text-align:center;'>Love in the Line of Fire</h1>", unsafe_allow_html=True)
+    st.markdown(
+        "<h1 style='text-align:center;'>Love in the Line of Fire</h1>",
+        unsafe_allow_html=True
+    )
 
 elif st.session_state.page == "Journey":
     st.markdown("<h1>Journey of a Young Mind</h1>", unsafe_allow_html=True)
     st.write("📅 **Will be uploaded on 14th Feb, 2026**")
-
